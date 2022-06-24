bf38c289e244ba79bb55652d9516ff51e86dcae7
Daniil Semenov <daniils2648@gmail.com>
2022-06-24 19:18:07 +0300

    added login, logout, reg func

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'accounts'
 
 urlpatterns = [
     path('login', views.user_login, name='login'),
     path('registration', views.user_registration, name='registration'),
+    path('logout', views.user_logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,63 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 
 def user_login(request):
+    """User login"""
     if request.method == 'POST':
         login = request.POST.get('login')
         password = request.POST.get('password')
-        return HttpResponseRedirect('/')
+        user = authenticate(request, username=login, password=password)
+        if user is not None:
+            login(request, user)
+        return HttpResponseRedirect(reverse('main:home'))
     else:
         return render(request, 'accounts/login.html')
 
 
+def user_logout(request):
+    """User logout"""
+    logout(request)
+    return HttpResponseRedirect(reverse('main:home'))
+
+
 def user_registration(request):
-    return render(request, 'accounts/registration.html')
+    """
+    Create new user
+    """
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('password_confirm')
+        try:
+            if password == confirm_password:
+                user = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password,
+                    first_name=first_name,
+                    last_name=last_name,
+                )
+                user.save()
+                login(request, user)
+                return HttpResponseRedirect(reverse('main:home'))
+            else:
+                context = {
+                    'password': 'Пароли не совпадают',
+                    'username': None,
+                }
+                return render(request, 'accounts/registration.html', context)
+        except ValueError:
+            context = {
+                'username': 'Введите имя пользователя',
+                'password': None,
+            }
+            return render(request, 'accounts/registration.html', context)
+    else:
+        return render(request, 'accounts/registration.html')
