33381ce268e0efdfe45e4dc83248550933078aa4
Daniil Semenov <daniils2648@gmail.com>
2022-06-24 16:03:53 +0300

    added new styles

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
 
 def user_login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        password = request.POST.get('password')
+        return HttpResponseRedirect('/')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def user_registration(request):
