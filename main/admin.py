c6ccc136de560c5754d549e46092f9e1cfe6ba1d
Daniil Semenov <daniils2648@gmail.com>
2022-07-05 16:37:41 +0300

    little changes

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Dialogue
+from .models import Dialogue, Post, Comment, Image
 
 admin.site.register(Dialogue)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Image)
--- main/models.py
+++ main/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 
 class Dialogue(models.Model):
-    text = models.TextField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=255)
 
     def __str__(self):
         return self.text
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    likes = models.IntegerField()
+    pud_date = models.DateTimeField(default=timezone.now())
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', null=True)
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='image', null=True)
+    
